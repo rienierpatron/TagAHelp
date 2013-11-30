d22e3bebdfbeb2efa6b8bfd6da720c2293a21bc4
rienierpatron <rienierpatron@gmail.com>
2013-11-30 18:49:25 +0800

    untracked

---
+++ protected/controllers/FundsController.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+class FundsController extends Controller
+{
+	public function actionIndex(){
+		if(isset($_SESSION['token'])){
+			$owned = Communities::getOwnedCommunities();
+			$communities = Communities::getCommunities();
+			$this->render('index',array('community'=>$communities,'owned'=>$owned));
+		}else{
+			$this->redirect(array('site/index'));
+		}
+	}
+	public function actionDashboard($id){
+		$details = Communities::getDetails($id);
+		$this->render('dashboard',array('detail'=>$details));
+	}
+	public function actionDetails($id){
+		$details = Communities::getDetails($id);
+		$this->render('view',array('detail'=>$details));
+	}
+}
+?>
