4cdbb05e4101a9569661f68ff6cb19729bbc3a0d
stephenricks <patrick.21.kenneth@gmail.com>
2013-11-30 20:51:28 +0800

    pushing editor

--- protected/views/community/dashboard.php
+++ protected/views/community/dashboard.php
@@ -44,9 +44,27 @@
 	    	<div class="widget-container fluid-height">
 	            <div class="heading">
 	                <i class="icon-list-alt"></i>Where Does Donations Go?
+	                <input type='button' id='edit-allotment' class='btn btn-xs btn-danger' value='EDIT'></div>
 	            </div>
 	            <div class="widget-content padded">
-	            	<form name="frmFunds" method="POST" action="<?php $this->createUrl(''); ?>">
+	            	<form name="frmFunds" method="POST" action="<?php $this->createUrl('funds/set'); ?>">
+	            	<table class="table table-filters" >
+		                <tbody id="fund-allotment">
+		               		<tr>
+		                		<th><b>Particulars</b></th>
+		                		<th><b>%</b></th>
+		                	</tr>
+		                	<tr>
+		                		<td>Tagbond(Service Fee)</td>
+		                		<td>20</td>
+		                	</tr>
+		                	<tr>
+		                		<td>Others</b></td>
+		                		<td>80</td>
+		                	</tr>
+		                </tbody>
+		            </table>
+	            	</form>
 	            </div>
 	    </div>
 	</div>
@@ -92,4 +110,53 @@
 	      	$('.location-not-found').show();
 	    }
 	});
+
+
+	//Allotment
+	var old_allotment = $("#fund-allotment").html();	
+
+	$("#edit-allotment").click(function(){
+
+		var elems = $('#fund-allotment > tr');
+		$("#edit-allotment").hide();
+
+		for(i=2;i<elems.length;i++){
+			var cell_unit = $(elems[i].cells);
+
+			//allotments[] = cell_unit[1].innerText
+			cell_unit[0].innerHTML = '<input type="text" class="form-control col-md-12" value="'+cell_unit[0].innerText+'"/>';
+			cell_unit[1].innerHTML = '<input type="text" class="form-control col-md-12" value="'+cell_unit[1].innerText+'"/>';
+
+		}
+
+		
+		$("#fund-allotment").after("<input type='button' value='Add new field' id='add-fund-field' class='btn btn-xs btn-info'/><input type='button' value='Save Changes' id='save-field' class='btn btn-xs btn-warning'/> <input type='button' value='Cancel' id='cancel-fund-allotment' class='btn btn-xs btn-primary'/>");
+		$("#add-fund-field").click(function(){
+			var textfield = '<tr><td><input type="text" class="form-control col-md-12" /></td><td><input type="text" class="form-control col-md-12" /></td></tr>'
+			$("#fund-allotment").append(textfield);
+		});
+		$("#cancel-fund-allotment").click(function(){
+			$("#edit-allotment").show();
+			$('#add-fund-field,#save-field,#cancel-fund-allotment').remove();
+			$("#fund-allotment").empty().append(old_allotment);
+		});
+
+		$("#save-field").click(function(){
+			var fields = {};
+			var elems = $('#fund-allotment > tr');
+			
+			for(i=2;i<elems.length;i++){
+				var cell_unit = $(elems[i].cells);
+				
+				fields[$(cell_unit[0]).find('input').val()] = $(cell_unit[1]).find('input').val();
+				
+			}
+
+
+		console.log(fields);
+
+		});
+	})
+
+	
 </script>
