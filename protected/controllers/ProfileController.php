dceb53de8251bd1f5a44348bc45f523d4665e046
jelladivina <jelladivina@ubuntu.ubuntu-domain>
2013-11-30 17:30:16 +0800

    profile page

--- protected/controllers/ProfileController.php
+++ protected/controllers/ProfileController.php
@@ -4,10 +4,8 @@ class ProfileController extends Controller
 {
 	public function actionIndex(){
 		if(isset($_SESSION['token'])){
-			
 			$info = Profile::getUserData();
 			$this->render('index',array('info'=>$info));
-			
 		}else{
 			$this->redirect(array('site/index'));
 		}
